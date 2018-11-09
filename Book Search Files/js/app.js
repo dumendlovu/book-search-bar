c038afea25a408c54e4ce27541093e35715b2c72
Unknown <dumendlovu@gmail.com>
2018-11-09 14:48:49 +0200

    data entry

--- Book Search Files/js/app.js
+++ Book Search Files/js/app.js
@@ -43,19 +43,100 @@ var shelf = new Vue({
         goodReadsLink:'https://www.goodreads.com/book/show/27161156-hillbilly-elegy'
       },
       {
-        title:"Killing the Rising Sun: How America Vanquished World War II Japan ",
+        title:"Killing the Rising Sun: How America Vanquished World War II Japan",
         author:"Bill O'Reilly",
         genres:'history critique',
         imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649169/bc-killing_the_rising_sun_qw2dcx.jpg',
         amazonLink:'https://www.amazon.com/Killing-Rising-Sun-Vanquished-OReillys/dp/1627790624',
         goodReadsLink:'https://www.goodreads.com/book/show/29370481-killing-the-rising-sun'
-      },{
+      },
+      {
         title:"The Black Widow",
         author:"Daniel Silva",
         genres:'thriller novel',
         imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649174/bc-the_black_widow_esmzpi.jpg',
         amazonLink:'https://www.amazon.com/Black-Widow-Novel-Gabriel-Allon/dp/1443456489',
         goodReadsLink:'https://www.goodreads.com/book/show/27213121-the-black-widow'
+      },
+      {
+        title:"Night School: A Jack Reacher Novel",
+        author:"Lee Child",
+        genres:'fiction novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649205/bc-the_jack_reacher_thriller_xwdyi7.jpg',
+        amazonLink:'https://www.amazon.com/Night-School-Jack-Reacher-Novel/dp/0804178828',
+        goodReadsLink:'https://www.goodreads.com/book/show/28077464-night-school'
+      },
+      {
+        title:"The Last Mile",
+        author:"David Baldacci",
+        genres:'crimeDrama novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649196/bc-the_last_mile_fqj7tu.jpg',
+        amazonLink:'https://www.amazon.com/Last-Mile-Memory-Man/dp/1455586447',
+        goodReadsLink:'https://www.goodreads.com/book/show/26245853-the-last-mile'
+      },
+      {
+        title:"The Whistler",
+        author:"John Grisham",
+        genres:'crimeDrama novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649194/bc-the_whistler_ofw7y6.jpg',
+        amazonLink:'https://www.amazon.com/Whistler-Novel-John-Grisham/dp/1101967684',
+        goodReadsLink:'https://www.goodreads.com/book/show/29354916-the-whistler'
+      },
+      {
+        title:"Truly Madly Guilty",
+        author:"Liane Moriarty",
+        genres:'fiction novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649201/bc-truly_madly_guilty_zlsdzb.jpg',
+        amazonLink:'https://www.amazon.com/Truly-Madly-Guilty-Liane-Moriarty/dp/1250069807',
+        goodReadsLink:'https://www.goodreads.com/book/show/26247008-truly-madly-guilty'
+      },
+      {
+        title:"When Breath Becomes Air",
+        author:"Paul Kalanithi",
+        genres:'autoBiograpghy memoir',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649191/bc-when_breath_becomes_air_gi50fe.jpg',
+        amazonLink:'https://www.amazon.com/When-Breath-Becomes-Paul-Kalanithi/dp/081298840X',
+        goodReadsLink:'https://www.goodreads.com/book/show/25899336-when-breath-becomes-air'
+      },
+      {
+        title:"Fool Me Once",
+        author:"Harlan Coben",
+        genres:'crimeDrama thriller novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649187/bc-fool_me_once_mcfhvf.jpg',
+        amazonLink:'https://www.amazon.com/Fool-Me-Once-Harlan-Coben/dp/1501217615',
+        goodReadsLink:'https://www.goodreads.com/book/show/26109394-fool-me-once'
+      },
+      {
+        title:"Crisis of Character",
+        author:"Gary. J Byrne",
+        genres:'autoBiograpghy history memoir',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649167/bc-crisis_of_character_zubxes.jpg',
+        amazonLink:'https://www.amazon.com/Summary-Crisis-Character-Discloses-Experience/dp/1717079032',
+        goodReadsLink:'https://www.goodreads.com/book/show/31572651-summary-of-crisis-of-character'
+      },
+      {
+        title:"The Wrong Side of Goodbye ",
+        author:"Michael Connelly",
+        genres:'crimeDrama thriller novel',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649169/bc-wrong_side_of_goodbye_ey7p5w.jpg',
+        amazonLink:'https://www.amazon.com/Wrong-Goodbye-Harry-Bosch-Novel/dp/1455524212',
+        goodReadsLink:'https://www.goodreads.com/book/show/29154543-the-wrong-side-of-goodbye'
+      },
+      {
+        title:"The Magnolia Story ",
+        author:"Chip and Joanna Gaines",
+        genres:'autoBiograpghy history memoir',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649186/bc-magnolia_story_txiz5r.jpg',
+        amazonLink:'https://www.amazon.com/Summary-Crisis-Character-Discloses-Experience/dp/1717079032',
+        goodReadsLink:'https://www.goodreads.com/book/show/31572651-summary-of-crisis-of-character'
+      },
+      {
+        title:"The Nest",
+        author:"Cynthia D'Aprix Sweeney",
+        genres:'autoBiograpghy romance memoir',
+        imgLink:'http://res.cloudinary.com/dtj4lxtyr/image/upload/v1519649188/bc-the_nest_dp5xhm.jpg',
+        amazonLink:'https://www.amazon.com/Nest-Cynthia-DAprix-Sweeney/dp/0062414224',
+        goodReadsLink:'https://www.goodreads.com/book/show/25781157-the-nest'
       }
     ],
     bookTemplate: ''
@@ -66,7 +147,7 @@ var shelf = new Vue({
       var newHTML = '';
 
       for(var i = 0; i < list.length; i++){
-        newHTML = newHTML + '<article class="col-md-3 col-sm-6 filterDiv '+list[i].genres+'"><img src="'+list[i].imgLink+'" alt="'+list[i].title+'"/><p class="text-center">'+list[i].title+'<br><span>'+list[i].author+'</span></p><button class="btn btn-default btn-warning"><a href="'+list[i].amazonLink+'" target="_blank"><ion-icon name="cash"></ion-icon> Buy on Amazon</a></button><button class="btn btn-default btn-primary"><a href="'+list[i].goodReadsLink+'" target="_blank"><ion-icon name="alert"></ion-icon> Find it on GoodReads</a></button></article>';
+        newHTML = newHTML + '<article class="col-md-3 col-sm-6 filterDiv '+list[i].genres+'"><img src="'+list[i].imgLink+'" alt="'+list[i].title+'"/><p class="text-center">'+list[i].title+'<br><span>'+list[i].author+'</span></p><button class="btn btn-default btn-info"><a href="'+list[i].amazonLink+'" target="_blank"><ion-icon name="cash"></ion-icon> Buy on Amazon</a></button><button class="btn btn-default btn-secondary"><a href="'+list[i].goodReadsLink+'" target="_blank"><ion-icon name="alert"></ion-icon> Find it on GoodReads</a></button></article>';
       }
       this.bookTemplate = newHTML;
       //console.log(newHTML);
